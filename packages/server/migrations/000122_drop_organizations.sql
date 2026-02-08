a5d3b8d3291a2c9a3618fc9de2577a62beddcfa4
Craig <craig@Craigs-Mac-Studio.local>
2026-02-08 09:46:22 -0600

    fix scripts

--- packages/server/migrations/000122_drop_organizations.sql
+++ packages/server/migrations/000122_drop_organizations.sql
@@ -1,3 +1,12 @@
+-- Drop view that depends on posts.organization_name
+DROP VIEW IF EXISTS listing_reports_with_details;
+
+-- Drop tables that FK to organizations
+DROP TABLE IF EXISTS business_organizations;
+
+-- Drop stale search function that references organizations
+DROP FUNCTION IF EXISTS search_organizations_by_similarity(vector, double precision, integer);
+
 -- Remove FK constraints referencing organizations
 ALTER TABLE posts DROP COLUMN IF EXISTS organization_id;
 ALTER TABLE posts DROP COLUMN IF EXISTS organization_name;
@@ -7,4 +16,4 @@ ALTER TABLE locations DROP COLUMN IF EXISTS organization_id;
 DROP TABLE IF EXISTS tags_on_organizations;
 
 -- Drop the organizations table itself
-DROP TABLE IF EXISTS organizations;
+DROP TABLE IF EXISTS organizations CASCADE;
